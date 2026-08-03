8dcde05f228c4ecd11d2d8436302c222d0838b1b
Akira Yamanashi <me@jameslin.info>
2026-08-03 16:39:32 -0400

    add menu function outline

--- src/content/main.ts
+++ src/content/main.ts
@@ -10,5 +10,44 @@ const hint_expand = 'Shift + right-click forces default menu'
 window.addEventListener('contextmenu', oncontextmenu, true)
 
 function oncontextmenu(e: MouseEvent): void {
-    // something goes here, but i'm procrasinating
+    if (e.shiftKey) return
+    const path = e.composedPath()
+    for (const node of path) {
+        if (node instanceof HTMLElement) {
+            if (node.isContentEditable) return
+            const tag = node.tagName
+            if (tag === 'INPUT' || tag == 'TEXTAREA' || tag === 'SELECT') return
+        }
+    }
+    
+    const media = findMedia(e, path)
+    const link = (path.find((n) => n instanceof HTMLAnchorElement && n.href) as HTMLAnchorElement | undefined ) ?? null
+    const selection = String(getSelection() ?? '').trim()
+
+    e.preventDefault()
+    e.stopImmediatePropagation()
+
+    if (media instanceof HTMLImageElement) {
+        showMenu(e.clientX, e.clientY, {
+            entries: iamageEntries(media),
+            more: mediaMoreEntries(link, imageMoreExtras(media)),
+            hint: hint_more,
+            expandedHint: hint_expand,
+        })
+    } else if (media instanceof HTMLVideoElement) {
+        showMenu(e.clientX, e.clientY, {
+            entries: videoEntries(media),
+            more: mediaMoreEntries(link, videoMoreExtras(media)),
+            hint: hint_more,
+            expandedHint: hint_expand,
+        })
+    } else {
+        showMenu(e.clientX, e.clientY, {
+            nav: navButtons(),
+            entries: pageEntries(link, selection),
+            more: pageMoreEntries(),
+            hint: hint_more,
+            expandedHint: hint_expand,
+        })
+    }
 }
