f5641e45f5f5541fdfd9136def76fd61c0b0c744
Akira Yamanashi <me@jameslin.info>
2026-08-03 20:30:21 -0400

    sw constants and framework

--- src/background/sw.ts
+++ src/background/sw.ts
@@ -1,1 +1,4 @@
 // The service worker YAYYYY
+import { port_name } from "../shared/protocol";
+import type { JobRequest, OffscreenEvnt,OffscreenReq, PortMessageFrom, PortMessageTo } from "../shared/protocol";
+const offscreenurl = 'src/offscreen/offscreen.html'
