3de242ee66b82997e521b7bb1fc6d117b45af7e2
Akira Yamanashi <me@jameslin.info>
2026-08-09 19:21:46 -0400

    version bump to 1.1.2

--- manifest.config.ts
+++ manifest.config.ts
@@ -3,7 +3,7 @@ import {defineManifest} from '@crxjs/vite-plugin'
 export default defineManifest({
     name: "rightweak",
     manifest_version: 3,
-    version: '1.1.1',
+    version: '1.1.2',
     description: 'A Chrome extension that commandeers the right-click context menu for additional user friendliness.',
     icons: {
         16: 'icons/icon16.png',
